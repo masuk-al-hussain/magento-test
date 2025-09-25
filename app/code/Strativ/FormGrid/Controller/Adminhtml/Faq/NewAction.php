9e345ee40be4af916f30e55077d77ccdf65db4d1
masuk-al-hussain <masuk.hussain@strativ.se>
2025-09-25 11:44:58 +0600

    add new button in grid

---
+++ app/code/Strativ/FormGrid/Controller/Adminhtml/Faq/NewAction.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Strativ\FormGrid\Controller\Adminhtml\Faq;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+class NewAction extends Action implements HttpGetActionInterface
+{
+    public const ADMIN_RESOURCE = 'Strativ_FormGrid::faq_save';
+
+    /** @var PageFactory */
+    protected $pageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        Context     $context,
+        PageFactory $pageFactory,
+    )
+    {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * Index action returns page
+     *
+     * @return Page
+     */
+    public function execute(): Page
+    {
+        $page = $this->pageFactory->create();
+        $page->setActiveMenu('Strativ_FormGrid::faq');
+        $page->getConfig()->getTitle()->prepend(__('New FAQ'));
+
+        return $page;
+    }
+}
