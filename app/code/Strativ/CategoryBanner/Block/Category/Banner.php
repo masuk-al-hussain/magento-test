defd0247dd5b3922ddd51a8b35a8f4308e2afa05
masuk-al-hussain <masuk.hussain@strativ.se>
2025-12-24 14:15:36 +0600

    refactor banner block

--- app/code/Strativ/CategoryBanner/Block/Category/Banner.php
+++ app/code/Strativ/CategoryBanner/Block/Category/Banner.php
@@ -4,6 +4,7 @@ namespace Strativ\CategoryBanner\Block\Category;
 
 use Magento\Catalog\Block\Category\View;
 use Magento\Catalog\Model\Category;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Element\Template\Context;
 use Magento\Catalog\Model\Layer\Resolver;
 use Magento\Framework\Registry;
@@ -12,44 +13,65 @@ use Magento\Catalog\Helper\Category as CategoryHelper;
 
 class Banner extends View
 {
-    protected $storeManager;
+    /** @var StoreManagerInterface */
+    protected StoreManagerInterface $storeManager;
 
+    /**
+     * @param Context $context
+     * @param Resolver $layerResolver
+     * @param Registry $registry
+     * @param CategoryHelper $categoryHelper
+     * @param StoreManagerInterface $storeManager
+     * @param array $data
+     */
     public function __construct(
-        Context $context,
-        Resolver $layerResolver,
-        Registry $registry,
-        CategoryHelper $categoryHelper,
+        Context               $context,
+        Resolver              $layerResolver,
+        Registry              $registry,
+        CategoryHelper        $categoryHelper,
         StoreManagerInterface $storeManager,
-        array $data = []
-    ) {
+        array                 $data = []
+    )
+    {
         $this->storeManager = $storeManager;
         parent::__construct($context, $layerResolver, $registry, $categoryHelper, $data);
     }
 
-    public function getCurrentCategory()
+    /**
+     * Get the current category from the registry
+     *
+     * @return Category|null
+     */
+    public function getCurrentCategory(): ?Category
     {
         return $this->_coreRegistry->registry('current_category');
     }
 
-    public function getBannerImage()
+    /**
+     * Get the banner image URL for the current category
+     *
+     * @return string|null
+     * @throws NoSuchEntityException
+     */
+    public function getBannerImage(): ?string
     {
         $category = $this->getCurrentCategory();
         if ($category && $category->getCategoryBannerImage()) {
             $imageValue = $category->getCategoryBannerImage();
-            
-            if (strpos($imageValue, 'http') === 0) {
+
+            if (str_starts_with($imageValue, 'http')) {
                 return $imageValue;
             }
-            
+
             $baseUrl = $this->storeManager->getStore()->getBaseUrl();
-            
-            if (strpos($imageValue, '/media/') === 0) {
+
+            if (str_starts_with($imageValue, '/media/')) {
                 return $baseUrl . ltrim($imageValue, '/');
             }
-            
+
             $mediaUrl = $this->storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
-            
-            if (strpos($imageValue, 'catalog/category/') === 0) {
+
+            if (str_starts_with($imageValue, 'catalog/category/')) {
                 return $mediaUrl . $imageValue;
             } else {
                 return $mediaUrl . 'catalog/category/' . $imageValue;
@@ -58,12 +80,23 @@ class Banner extends View
         return null;
     }
 
-    public function hasBannerImage()
+    /**
+     * Check if the current category has a banner image
+     *
+     * @return bool
+     * @throws NoSuchEntityException
+     */
+    public function hasBannerImage(): bool
     {
         return $this->getBannerImage() !== null;
     }
 
-    public function getCategoryName()
+    /**
+     * Get the name of the current category
+     *
+     * @return string
+     */
+    public function getCategoryName(): string
     {
         $category = $this->getCurrentCategory();
         return $category ? $category->getName() : '';
